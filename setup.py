7b04f7f96a69dbe6695db32efe2798b16b47f46b
René Devichi <rene.devichi@gmail.com>
2019-12-25 12:41:46 +0100

    version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="netatmo",
-    version="0.0.1",
+    version="0.0.2",
     description="Python3 API for the Netatmo Weather Station",
     long_description=long_description,
     long_description_content_type='text/markdown',  # Optional (see note above)
